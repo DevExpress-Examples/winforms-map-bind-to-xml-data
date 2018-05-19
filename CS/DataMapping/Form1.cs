742c27b91df3f9c51d258b667a61c40a45e87ad3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/DataMapping/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using DevExpress.Utils;
+using DevExpress.XtraMap;
+
+namespace DataMapping {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        VectorItemsLayer itemsLayer;
+        const string xmlFilePath = @"..\..\Data\Ships.xml";
+        const string imageFilePath = @"..\..\Image\Ship.png";
+
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            // Create a map control with initial settings and add it to the form.
+            MapControl map = new MapControl() { ZoomLevel = 6, Dock = DockStyle.Fill };
+            map.CenterPoint = new GeoPoint(-35, 145);
+            this.Controls.Add(map);
+
+            // Create a layer to load image tiles from MS Bing.
+            ImageTilesLayer tileLayer = new ImageTilesLayer();
+            BingMapDataProvider bingProvider = new BingMapDataProvider();
+            bingProvider.BingKey = "Insert Your Bing Key";
+            tileLayer.DataProvider = bingProvider;
+            map.Layers.Add(tileLayer);
+
+            // Create a vector items layer.
+            itemsLayer = new VectorItemsLayer();
+
+            // Specify mappings for Latitude and Longitude coordinates.
+            itemsLayer.Mappings.Latitude = "Latitude";
+            itemsLayer.Mappings.Longitude = "Longitude";
+
+            // Specify attribute mappings for ship name, year and description.
+            itemsLayer.AttributeMappings.Add(
+                new MapItemAttributeMapping() { Member = "Name", Name = "Name" });
+            itemsLayer.AttributeMappings.Add(
+                new MapItemAttributeMapping() { Member = "Year", Name = "Year" });
+            itemsLayer.AttributeMappings.Add(
+                new MapItemAttributeMapping() { Member = "Description", Name = "Desc" });
+
+            // Specify a datasource. 
+            itemsLayer.DataSource = LoadShipsFromXML(xmlFilePath);
+
+            // Specify an image for generated vector items.              
+            ImageCollection imageCollection = new ImageCollection();
+            Bitmap image = new Bitmap(imageFilePath);
+            imageCollection.ImageSize = new Size(40, 40);
+            imageCollection.Images.Add(image);
+            itemsLayer.ItemImageIndex = 0;
+            map.ImageList = imageCollection;
+
+            // Specify tooltip contents.
+            map.ToolTipController = new ToolTipController() { AllowHtmlText = true };
+            itemsLayer.ToolTipPattern = "<b>{Name} ({Year})</b> \r\n{Desc}";
+
+            // Add a vector items layer to the map control.
+            map.Layers.Add(itemsLayer);
+        }
+
+
+        public class ShipInfo {
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+            public string Name { get; set; }
+            public string Year { get; set; }
+            public string Description { get; set; }
+        }
+
+        private List<ShipInfo> LoadShipsFromXML(string filePath) {
+            List<ShipInfo> ships = new List<ShipInfo>();
+
+            // Load an XML document from the specified file path.
+            XDocument document = XDocument.Load(filePath);
+            if (document != null) {
+                foreach (XElement element in document.Element("Ships").Elements()) {
+                    // Load ShipInfo values and add them to the list.
+                    double latitude = Convert.ToDouble(element.Element("Latitude").Value, CultureInfo.InvariantCulture);
+                    double longitude = Convert.ToDouble(element.Element("Longitude").Value, CultureInfo.InvariantCulture);
+                    string name = element.Element("Name").Value;
+                    string description = element.Element("Description").Value;
+                    string year = element.Element("Year").Value;
+
+                    ships.Add(new ShipInfo() { Latitude = latitude, Longitude = longitude, Description = description, Name = name, Year = year });
+                }
+            }
+
+            return ships;
+        }
+
+    }
+
+}
+
---
+++ VB/New folder/DataMapping/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Globalization
+Imports System.Windows.Forms
+Imports System.Xml.Linq
+Imports DevExpress.Utils
+Imports DevExpress.XtraMap
+
+Namespace DataMapping
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private itemsLayer As VectorItemsLayer
+		Private Const xmlFilePath As String = "..\..\Data\Ships.xml"
+		Private Const imageFilePath As String = "..\..\Image\Ship.png"
+
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+
+			' Create a map control with initial settings and add it to the form.
+			Dim map As New MapControl() With {.ZoomLevel = 6, .Dock = DockStyle.Fill}
+			map.CenterPoint = New GeoPoint(-35, 145)
+			Me.Controls.Add(map)
+
+			' Create a layer to load image tiles from MS Bing.
+			Dim tileLayer As New ImageTilesLayer()
+			Dim bingProvider As New BingMapDataProvider()
+			bingProvider.BingKey = "Insert Your Bing Key"
+			tileLayer.DataProvider = bingProvider
+			map.Layers.Add(tileLayer)
+
+			' Create a vector items layer.
+			itemsLayer = New VectorItemsLayer()
+
+
+			' Specify a datasource. 
+			itemsLayer.DataSource = LoadShipsFromXML(xmlFilePath)
+
+			' Specify mappings for Latitude and Longitude coordinates.
+			itemsLayer.Mappings.Latitude = "Latitude"
+			itemsLayer.Mappings.Longitude = "Longitude"
+
+			' Specify an image for generated vector items.              
+			Dim imageCollection As New ImageCollection()
+			Dim image As New Bitmap(imageFilePath)
+			imageCollection.ImageSize = New Size(40, 40)
+			imageCollection.Images.Add(image)
+			itemsLayer.ItemImageIndex = 0
+			map.ImageList = imageCollection
+
+
+			' Specify additional mappings for ship name and description.
+			itemsLayer.AttributeMappings.Add(New MapItemAttributeMapping() With {.Member = "Name", .Name = "Name"})
+			itemsLayer.AttributeMappings.Add(New MapItemAttributeMapping() With {.Member = "Year", .Name = "Year"})
+			itemsLayer.AttributeMappings.Add(New MapItemAttributeMapping() With {.Member = "Description", .Name = "Desc"})
+
+			' Specify tooltip contents.
+			map.ToolTipController = New ToolTipController() With {.AllowHtmlText = True}
+			itemsLayer.ToolTipPattern = "<b>{Name} ({Year})</b> " & Constants.vbCrLf & "{Desc}"
+
+			' Add a vector items layer to the map control.
+			map.Layers.Add(itemsLayer)
+		End Sub
+
+
+		Public Class ShipInfo
+			Private privateLatitude As Double
+			Public Property Latitude() As Double
+				Get
+					Return privateLatitude
+				End Get
+				Set(ByVal value As Double)
+					privateLatitude = value
+				End Set
+			End Property
+			Private privateLongitude As Double
+			Public Property Longitude() As Double
+				Get
+					Return privateLongitude
+				End Get
+				Set(ByVal value As Double)
+					privateLongitude = value
+				End Set
+			End Property
+			Private privateName As String
+			Public Property Name() As String
+				Get
+					Return privateName
+				End Get
+				Set(ByVal value As String)
+					privateName = value
+				End Set
+			End Property
+			Private privateYear As String
+			Public Property Year() As String
+				Get
+					Return privateYear
+				End Get
+				Set(ByVal value As String)
+					privateYear = value
+				End Set
+			End Property
+			Private privateDescription As String
+			Public Property Description() As String
+				Get
+					Return privateDescription
+				End Get
+				Set(ByVal value As String)
+					privateDescription = value
+				End Set
+			End Property
+		End Class
+
+		Private Function LoadShipsFromXML(ByVal filePath As String) As List(Of ShipInfo)
+			Dim ships As New List(Of ShipInfo)()
+
+			' Load an XML document from the specified file path.
+			Dim document As XDocument = XDocument.Load(filePath)
+			If document IsNot Nothing Then
+				For Each element As XElement In document.Element("Ships").Elements()
+					' Load ShipInfo values and add them to the list.
+					Dim latitude As Double = Convert.ToDouble(element.Element("Latitude").Value, CultureInfo.InvariantCulture)
+					Dim longitude As Double = Convert.ToDouble(element.Element("Longitude").Value, CultureInfo.InvariantCulture)
+					Dim name As String = element.Element("Name").Value
+					Dim description As String = element.Element("Description").Value
+					Dim year As String = element.Element("Year").Value
+
+					ships.Add(New ShipInfo() With {.Latitude = latitude, .Longitude = longitude, .Description = description, .Name = name, .Year = year})
+				Next element
+			End If
+
+			Return ships
+		End Function
+
+	End Class
+
+End Namespace
+
