ef1e293c0d2b02738a2f4f758d30ef76993d4dd8
DevExpressExampleBot <fakeEmail>
2022-08-19 12:12:01 +0400

    Source auto update [skip ci]

--- VB/WinForms_MapControl_ListAdapter/Form1.vb
+++ VB/WinForms_MapControl_ListAdapter/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Data
 Imports System.Drawing
@@ -10,127 +10,96 @@ Imports DevExpress.Utils
 Imports DevExpress.XtraMap
 
 Namespace WinForms_MapControl_ListAdapter
-    Partial Public Class Form1
-        Inherits Form
 
-        Private Const bingKey As String = "YOUR_BING_MAPS_KEY_HERE"
-        Private Const xmlFilepath As String = "..\..\Data\Ships.xml"
-        Private Const imageFilepath As String = "..\..\Image\Ship.png"
+    Public Partial Class Form1
+        Inherits System.Windows.Forms.Form
+
+        Const bingKey As String = "YOUR_BING_MAPS_KEY_HERE"
+
+        Const xmlFilepath As String = "..\..\Data\Ships.xml"
+
+        Const imageFilepath As String = "..\..\Image\Ship.png"
 
         Public Sub New()
-            InitializeComponent()
-            InitializeMap()
+            Me.InitializeComponent()
+            Me.InitializeMap()
         End Sub
 
         Private Sub InitializeMap()
-'            #Region "#MapPreparation"
-            Dim data As Object = LoadData(xmlFilepath)
-
+'#Region "#MapPreparation"
+            Dim data As Object = Me.LoadData(WinForms_MapControl_ListAdapter.Form1.xmlFilepath)
             ' Create a map and data for it.
-            Dim map As New MapControl() With { _
-                .CenterPoint = New GeoPoint(-37.2, 143.2), _
-                .ZoomLevel = 5, _
-                .Dock = DockStyle.Fill, _
-                .ToolTipController = New ToolTipController() With {.AllowHtmlText = True}, _
-                .ImageList = LoadImage(imageFilepath) _
-            }
+            Dim map As DevExpress.XtraMap.MapControl = New DevExpress.XtraMap.MapControl() With {.CenterPoint = New DevExpress.XtraMap.GeoPoint(-37.2, 143.2), .ZoomLevel = 5, .Dock = System.Windows.Forms.DockStyle.Fill, .ToolTipController = New DevExpress.Utils.ToolTipController() With {.AllowHtmlText = True}, .ImageList = Me.LoadImage(WinForms_MapControl_ListAdapter.Form1.imageFilepath)}
             Me.Controls.Add(map)
-'            #End Region ' #MapPreparation
-
-            map.Layers.Add(New ImageLayer() With { _
-                .DataProvider = New BingMapDataProvider() With {.BingKey = bingKey} _
-            })
-'            #Region "#VectorData"
+'#End Region  ' #MapPreparation
+            map.Layers.Add(New DevExpress.XtraMap.ImageLayer() With {.DataProvider = New DevExpress.XtraMap.BingMapDataProvider() With {.BingKey = WinForms_MapControl_ListAdapter.Form1.bingKey}})
+'#Region "#VectorData"
             ' Create a vector layer.
-            map.Layers.Add(New VectorItemsLayer() With { _
-                .Data = CreateAdapter(data), _
-                .ToolTipPattern = "<b>{Name} ({Year})</b> " & ControlChars.CrLf & "{Description}", _
-                .ItemImageIndex = 0 _
-            })
-'            #End Region ' #VectorData
-
-'            #Region "#MiniMap"
+            map.Layers.Add(New DevExpress.XtraMap.VectorItemsLayer() With {.Data = Me.CreateAdapter(data), .ToolTipPattern = "<b>{Name} ({Year})</b> " & Global.Microsoft.VisualBasic.Constants.vbCrLf & "{Description}", .ItemImageIndex = 0})
+'#End Region  ' #VectorData
+'#Region "#MiniMap"
             ' Create a mini map and data for it.         
-            Dim miniMap As New MiniMap() With {.Alignment = MiniMapAlignment.BottomLeft}
-            miniMap.Layers.Add(New MiniMapImageTilesLayer() With { _
-                .DataProvider = New BingMapDataProvider() With {.BingKey = bingKey} _
-            })
-            miniMap.Layers.Add(New MiniMapVectorItemsLayer() With {.Data = CreateMiniMapAdapter(data)})
+            Dim miniMap As DevExpress.XtraMap.MiniMap = New DevExpress.XtraMap.MiniMap() With {.Alignment = DevExpress.XtraMap.MiniMapAlignment.BottomLeft}
+            miniMap.Layers.Add(New DevExpress.XtraMap.MiniMapImageTilesLayer() With {.DataProvider = New DevExpress.XtraMap.BingMapDataProvider() With {.BingKey = WinForms_MapControl_ListAdapter.Form1.bingKey}})
+            miniMap.Layers.Add(New DevExpress.XtraMap.MiniMapVectorItemsLayer() With {.Data = Me.CreateMiniMapAdapter(data)})
             map.MiniMap = miniMap
-'            #End Region ' #MiniMap
-
-'            #Region "#Legend"
+'#End Region  ' #MiniMap
+'#Region "#Legend"
             'Create a Legend containing images.
-            Dim legend As New ColorListLegend()
+            Dim legend As DevExpress.XtraMap.ColorListLegend = New DevExpress.XtraMap.ColorListLegend()
             legend.ImageList = map.ImageList
-            legend.CustomItems.Add(New ColorLegendItem() With { _
-                .ImageIndex = 0, _
-                .Text = "Shipwreck" _
-            })
+            legend.CustomItems.Add(New DevExpress.XtraMap.ColorLegendItem() With {.ImageIndex = 0, .Text = "Shipwreck"})
             map.Legends.Add(legend)
-'            #End Region ' #Legend
+'#End Region  ' #Legend
         End Sub
 
-        #Region "#CreateAdapter"
+'#Region "#CreateAdapter"
         ' Creates an adapter for the map's vector layer.
         Private Function CreateAdapter(ByVal source As Object) As IMapDataAdapter
-            Dim adapter As New ListSourceDataAdapter()
-
+            Dim adapter As DevExpress.XtraMap.ListSourceDataAdapter = New DevExpress.XtraMap.ListSourceDataAdapter()
             adapter.DataSource = source
-
             adapter.Mappings.Latitude = "Latitude"
             adapter.Mappings.Longitude = "Longitude"
-
-            adapter.AttributeMappings.Add(New MapItemAttributeMapping() With { _
-                .Member = "Name", _
-                .Name = "Name" _
-            })
-            adapter.AttributeMappings.Add(New MapItemAttributeMapping() With { _
-                .Member = "Year", _
-                .Name = "Year" _
-            })
-            adapter.AttributeMappings.Add(New MapItemAttributeMapping() With { _
-                .Member = "Description", _
-                .Name = "Description" _
-            })
-
+            adapter.AttributeMappings.Add(New DevExpress.XtraMap.MapItemAttributeMapping() With {.Member = "Name", .Name = "Name"})
+            adapter.AttributeMappings.Add(New DevExpress.XtraMap.MapItemAttributeMapping() With {.Member = "Year", .Name = "Year"})
+            adapter.AttributeMappings.Add(New DevExpress.XtraMap.MapItemAttributeMapping() With {.Member = "Description", .Name = "Description"})
             Return adapter
         End Function
-        #End Region ' #CreateAdapter
 
-        #Region "#CreateMiniMapAdapter"
+'#End Region  ' #CreateAdapter
+'#Region "#CreateMiniMapAdapter"
         ' Creates an adapter for the mini map's vector layer.
         Private Function CreateMiniMapAdapter(ByVal source As Object) As IMapDataAdapter
-            Dim adapter As New ListSourceDataAdapter()
-
+            Dim adapter As DevExpress.XtraMap.ListSourceDataAdapter = New DevExpress.XtraMap.ListSourceDataAdapter()
             adapter.DataSource = source
-
             adapter.Mappings.Latitude = "Latitude"
             adapter.Mappings.Longitude = "Longitude"
-
-            adapter.PropertyMappings.Add(New MapItemFillMapping() With {.DefaultValue = Color.Red})
-            adapter.PropertyMappings.Add(New MapItemStrokeMapping() With {.DefaultValue = Color.White})
-            adapter.PropertyMappings.Add(New MapItemStrokeWidthMapping() With {.DefaultValue = 2})
-            adapter.PropertyMappings.Add(New MapDotSizeMapping() With {.DefaultValue = 8})
-
-            adapter.DefaultMapItemType = MapItemType.Dot
-
+            adapter.PropertyMappings.Add(New DevExpress.XtraMap.MapItemFillMapping() With {.DefaultValue = System.Drawing.Color.Red})
+            adapter.PropertyMappings.Add(New DevExpress.XtraMap.MapItemStrokeMapping() With {.DefaultValue = System.Drawing.Color.White})
+            adapter.PropertyMappings.Add(New DevExpress.XtraMap.MapItemStrokeWidthMapping() With {.DefaultValue = 2})
+            adapter.PropertyMappings.Add(New DevExpress.XtraMap.MapDotSizeMapping() With {.DefaultValue = 8})
+            adapter.DefaultMapItemType = DevExpress.XtraMap.MapItemType.Dot
             Return adapter
         End Function
-        #End Region ' #CreateMiniMapAdapter
 
-        #Region "#LoadData"
+'#End Region  ' #CreateMiniMapAdapter
+'#Region "#LoadData"
         ' Loads data from a XML file.
-        Private Function LoadData(ByVal path As String) As List(Of ShipwreckData)
-            Return XDocument.Load(path).Element("Ships").Elements("Ship").Select(Function(e) New ShipwreckData(year:= Convert.ToInt32(e.Element("Year").Value, CultureInfo.InvariantCulture), name:= e.Element("Name").Value, description:= e.Element("Description").Value, latitude:= Convert.ToDouble(e.Element("Latitude").Value, CultureInfo.InvariantCulture), longitude:= Convert.ToDouble(e.Element("Longitude").Value, CultureInfo.InvariantCulture))).ToList()
+        Private Function LoadData(ByVal path As String) As List(Of WinForms_MapControl_ListAdapter.Form1.ShipwreckData)
+            Return System.Xml.Linq.XDocument.Load(CStr((path))).Element(CType(("Ships"), System.Xml.Linq.XName)).Elements("Ship").[Select](Function(e) New WinForms_MapControl_ListAdapter.Form1.ShipwreckData(year:=System.Convert.ToInt32(e.Element(CType(("Year"), System.Xml.Linq.XName)).Value, System.Globalization.CultureInfo.InvariantCulture), name:=e.Element(CType(("Name"), System.Xml.Linq.XName)).Value, description:=e.Element(CType(("Description"), System.Xml.Linq.XName)).Value, latitude:=System.Convert.ToDouble(e.Element(CType(("Latitude"), System.Xml.Linq.XName)).Value, System.Globalization.CultureInfo.InvariantCulture), longitude:=System.Convert.ToDouble(e.Element(CType(("Longitude"), System.Xml.Linq.XName)).Value, System.Globalization.CultureInfo.InvariantCulture))).ToList()
         End Function
 
         Public Class ShipwreckData
-            Public ReadOnly Property Year() As Integer
-            Public ReadOnly Property Name() As String
-            Public ReadOnly Property Description() As String
-            Public ReadOnly Property Latitude() As Double
-            Public ReadOnly Property Longitude() As Double
+
+            Public ReadOnly Property Year As Integer
+
+            Public ReadOnly Property Name As String
+
+            Public ReadOnly Property Description As String
+
+            Public ReadOnly Property Latitude As Double
+
+            Public ReadOnly Property Longitude As Double
 
             Public Sub New(ByVal year As Integer, ByVal name As String, ByVal description As String, ByVal latitude As Double, ByVal longitude As Double)
                 Me.Year = year
@@ -139,20 +108,18 @@ Namespace WinForms_MapControl_ListAdapter
                 Me.Latitude = latitude
                 Me.Longitude = longitude
             End Sub
-
         End Class
-        #End Region ' #LoadData
 
-        #Region "#LoadImage"
+'#End Region  ' #LoadData
+'#Region "#LoadImage"
         ' Loads an image to an image collection.
         Private Function LoadImage(ByVal path As String) As ImageCollection
-            Dim imageCollection As New ImageCollection()
-            Dim image As New Bitmap(path)
-            imageCollection.ImageSize = New Size(50, 50)
+            Dim imageCollection As DevExpress.Utils.ImageCollection = New DevExpress.Utils.ImageCollection()
+            Dim image As System.Drawing.Bitmap = New System.Drawing.Bitmap(path)
+            imageCollection.ImageSize = New System.Drawing.Size(50, 50)
             imageCollection.Images.Add(image)
             Return imageCollection
         End Function
-        #End Region ' #LoadImage
+'#End Region  ' #LoadImage
     End Class
-
 End Namespace
